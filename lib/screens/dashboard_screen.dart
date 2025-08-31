92b37bdf1a6df0dd289e696ba81561238c04c419
Seane <grcseane@gmail.com>
2025-08-31 15:08:48 +0800

    Incoming Packages redirect

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'inventory_screen.dart';
 import 'profile_screen.dart';
 import 'qr_screen.dart';
+import 'incoming_package.dart';
 import 'transactions_screen.dart';
 import '../widgets/custom_navbar.dart'; // adjust path if needed
 import '../widgets/screen_header.dart';
@@ -195,7 +196,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
                         Navigator.pushReplacement(
                           context,
                           MaterialPageRoute(
-                            builder: (_) => const TransactionsScreen(),
+                            builder: (_) => const IncomingPackage(),
                           ),
                         );
                       },
